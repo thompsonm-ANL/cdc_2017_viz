f1dd690cd25cce36be94f97444920f799fb551c3
mt <mt@localhost.localdomain>
2017-03-30 07:40:40 -0500

    thursday morning -- add graphing components and team configs

--- config.py
+++ config.py
@@ -2,8 +2,38 @@ prefix = "10.10.20."
 red_range = [1, 25]
 green_range = [50, 100]
 
+cdc = [
+    {"num": 1, "name": "Dakota State"},
+    {"num": 2, "name": "Governors State"},
+    {"num": 3, "name": "Indiana Tech"},
+    {"num": 4, "name": "Indiana University"},
+    {"num": 5, "name": "Iowa State"},
+    {"num": 6, "name": "John A Logan"},
+    {"num": 7, "name": "Kansas State"},
+    {"num": 8, "name": "Lewis"},
+    {"num": 9, "name": "Southern Methodist"},
+    {"num": 10, "name": "St. Johns"},
+    {"num": 11, "name": "Central Florida"},
+    {"num": 12, "name": "UIC"},
+    {"num": 13, "name": "UIUC"},
+    {"num": 14, "name": "Northern Iowa"},
+    {"num": 15, "name": "Wright State"},
+]
+
+blue = {
+    "File Server": "30",
+    "Active Directory": "40",
+    "HMI": "50",
+    "Mail Server": "60",
+    "Web Server": "70",
+    "ESXi" : "2",
+}
+
+def getPrefix(team_num):
+    return "10.0.%d0" % team_num
+
 def getNetwork(team_num):
-    return "10.0.%d0.0" % team_num
+    return "%s.%s" % (getPrefix(team_num), "0")
 
 def is_red(last_oct):
     if last_oct in range(red_range[0], red_range[1]):
@@ -14,3 +44,8 @@ def is_green(last_oct):
     if last_oct in range(green_range[0], green_range[1]):
         return True
     return False
+
+# this is gettable from twisted while we're running
+teamnets = []
+for i in range(1, 16):
+  teamnets.append(getNetwork(i))
--- sniff.py
+++ sniff.py
@@ -13,14 +13,20 @@ import teams
 import config
 import csv
 import json
+import networkx as nx
 
 log.startLogging(sys.stdout)
 
-num_teams = 15
+# num_teams = 15
+#
+# ts = teams.Teams()
+# for i in range(1, num_teams+1):
+#     team = teams.Team(i, config.getNetwork(i), "team %d" % i)
+#     ts.addTeam(team)
 
 ts = teams.Teams()
-for i in range(1, num_teams+1):
-    team = teams.Team(i, config.getNetwork(i), "team %d" % i)
+for t in config.cdc:
+    team = teams.Team(t["num"], config.getNetwork(t["num"]), t["name"])
     ts.addTeam(team)
 
 class SnifferService(TsharkService):
@@ -43,6 +49,40 @@ class Data(Resource):
     isLeaf = True
     def render_GET(self, request):
         return json.dumps(ts.dump())
+
+class Graph(Resource):
+    isLeaf = True
+    G = nx.Graph()
+    root = "Red Team"
+    G.add_node(root)
+    def render_GET(self, request):
+        for team in ts.teams:
+            G.add_node(team.name)
+            G.add_edge(root, team.name)
+            G[root][team.name]['weight'] = team.state.getAttackers()
+        thresholds = [200, 400, 600, 800]
+
+        low = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] < thresholds[0]]
+        lowmed = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] in range(thresholds[0], thresholds[1])]
+        med = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] in range(thresholds[1], thresholds[2])]
+        himed = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] in range(thresholds[2], thresholds[3])]
+        hi = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > thresholds[3]]
+        a = 0.7
+
+        pos = graphviz_layout(G, prog='twopi', args='')
+        plt.figure(figsize=(8, 8))
+        # nx.draw_networkx_nodes(G,pos,node_size=700)
+        nx.draw_networkx_edges(G, pos, edgelist=low, width=1, alpha=a)
+        nx.draw_networkx_edges(G, pos, edgelist=lowmed, width=2, alpha=a)
+        nx.draw_networkx_edges(G, pos, edgelist=med, width=3, edge_color="red", alpha=a)
+        nx.draw_networkx_edges(G, pos, edgelist=himed, width=4, edge_color="red", alpha=a)
+        nx.draw_networkx_edges(G, pos, edgelist=hi, width=6, edge_color="red", alpha=a)
+        # nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
+        nx.draw(G, pos, node_size=180, alpha=0.5, node_color="blue", with_labels=True)
+        plt.axis('equal')
+        plt.savefig('graph.png')
+        #plt.show()
+
 
 def main():
     iface = sys.argv[1]
