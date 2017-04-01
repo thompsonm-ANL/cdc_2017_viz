acc6bc52eab14ecccc0a09ee4ecde061d58ff378
thompsonm@anl.gov <thompsonm@anl.gov>
2017-04-01 07:16:04 -0500

    try except

--- sniff.py
+++ sniff.py
@@ -25,8 +25,11 @@ for t in config.cdc:
 class SnifferService(TsharkService):
     def packetReceived(self, packet):
         """Override the TsharkService method"""
-        log.msg("src ip: %s dst ip: %s" % (packet['ip'].src, packet['ip'].dst))
-        ts.slotPacket(packet)
+        try:
+          log.msg("src ip: %s dst ip: %s" % (packet['ip'].src, packet['ip'].dst))
+          ts.slotPacket(packet)
+        except:
+            pass
 
 class Data(Resource):
     isLeaf = True
